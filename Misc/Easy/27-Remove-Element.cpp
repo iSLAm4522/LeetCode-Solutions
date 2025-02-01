4d896a86d9de34d09dd7944a6631d6d0d9479143
Islam Madian <im1996@fayoum.edu.eg>
2025-02-01 07:23:45 +0200

    [27] [Time Beats: 100.00%] [Memory Beats: 97.57%] - LeetPush

---
+++ Misc/Easy/27-Remove-Element.cpp
@@ -0,0 +1,16 @@
+
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int len = nums.size();
+        int k = 0;
+        for (int i = 0; i < len; i++) {
+          if (nums[i] != val) {
+            nums[k++] = nums[i];
+          }
+        }
+        return k;
+    }
+};
