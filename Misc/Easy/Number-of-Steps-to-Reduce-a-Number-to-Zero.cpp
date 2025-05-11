b008cd9d30225415917c9418d7646a9be874baec
Islam Madian <im1996@fayoum.edu.eg>
2025-05-11 21:38:28 +0300

    [1342] [Time Beats: 100.00%] [Memory Beats: 45.30%] - LeetPush

---
+++ Misc/Easy/Number-of-Steps-to-Reduce-a-Number-to-Zero.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int cnt = 0;
+        while (num) {
+            if (!(num % 2))
+                num /= 2;
+            else
+                num -= 1;
+            cnt++;
+        }
+        return cnt;
+    }
+};
