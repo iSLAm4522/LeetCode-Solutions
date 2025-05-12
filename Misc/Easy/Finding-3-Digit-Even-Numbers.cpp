f5c8a6828c06cdefa2f97e547d7dc24cb8a2ade5
Islam Madian <im1996@fayoum.edu.eg>
2025-05-12 11:45:27 +0300

    [2094] [Time Beats: 58.45%] [Memory Beats: 30.63%] - LeetPush

--- Misc/Easy/Finding-3-Digit-Even-Numbers.cpp
+++ Misc/Easy/Finding-3-Digit-Even-Numbers.cpp
@@ -1,36 +1,27 @@
-
 class Solution {
 public:
     vector<int> findEvenNumbers(vector<int>& digits) {
+        vector<int> freq(10, 0);
+        for (int digit : digits) {
+            freq[digit]++;
+        }
+        
         vector<int> ans;
-        set<int> st;
-        int size = digits.size();
-        sort(digits.begin(), digits.end());
-        int i = 0;
-        while(digits[i] == 0)
-            i++;
-        for (; i < size; i++) {
-            int num = digits[i];
-            for (int j = 0; j < size; j++) {
-                if(i == j && digits[i] == digits[j])
-                    continue; 
-                num = num * 10 + digits[j];
-                for (int k = 0; k < size; k++) {
-                    if((k == i && digits[i] == digits[k])||(k == j && digits[k] == digits[j]))
-                        continue;
-                    num = num * 10 + digits[k];
-                    if(!(num % 2)){
-                        st.insert(num);
-                    }
-                    num -= digits[k];
-                    num /= 10;
-                }
-                num -= digits[j];
-                num /= 10;
+        for (int num = 100; num <= 998; num += 2) { 
+
+            int third = num % 10;        
+            int second = (num / 10) % 10;
+            int first = num / 100;       
+            
+            vector<int> curr_freq = freq;
+            
+            if (--curr_freq[first] >= 0 && 
+                --curr_freq[second] >= 0 && 
+                --curr_freq[third] >= 0) {
+                ans.push_back(num);
             }
         }
-        for (int ele : st)
-            ans.push_back(ele);
+        
         return ans;
     }
 };
