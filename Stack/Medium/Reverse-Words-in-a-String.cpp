cf722037e6e3a6ce377a0f79fadd1038ec2f55c5
Islam Madian <im1996@fayoum.edu.eg>
2025-05-22 01:42:51 +0300

    [151] [Time Beats: 100.00%] [Memory Beats: 72.14%] - LeetPush

---
+++ Stack/Medium/Reverse-Words-in-a-String.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+    string reverseWords(string str) {
+        stack<char> stk;
+        string ans = \\;
+        int i = 0, j = str.length() - 1;
+        while (str[i] == ' ')
+            i++;
+        while (str[j] == ' ')
+            j--;
+        str = str.substr(i, j - i + 1);
+        for (const char& ch : str){
+            if(ch == ' ' && stk.top() == ' ')
+                continue;
+            stk.push(ch);
+        }
+
+        while (!stk.empty()) {
+            string subStr = \\;
+            while (!stk.empty() && stk.top() != ' ') {
+                subStr = stk.top() + subStr;
+                stk.pop();
+            }
+            ans += subStr;
+            if(stk.size() > 1){
+                stk.pop();
+                ans+= \ \;
+            }
+        }
+        return ans;
+    }
+};
