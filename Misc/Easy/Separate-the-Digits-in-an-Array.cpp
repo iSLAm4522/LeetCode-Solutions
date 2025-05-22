6bb0a0d0e65c29790573831eee54af1a1b927ff5
Islam Madian <im1996@fayoum.edu.eg>
2025-05-22 21:11:03 +0300

    [2553] [Time Beats: 100.00%] [Memory Beats: 82.11%] - LeetPush

---
+++ Misc/Easy/Separate-the-Digits-in-an-Array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void separateDigitsHelper(vector<int>& vec, int num){
+        if(num == 0) return;
+        separateDigitsHelper(vec, num / 10);
+        vec.push_back(num%10);
+    }
+    vector<int> separateDigits(vector<int>& nums) {
+        vector<int> ans;
+        for (int num : nums)
+            separateDigitsHelper(ans, num);
+        return ans;
+    }
+};
