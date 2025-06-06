aead69a4f6d371dedc41730313ae5e93388c6627
Islam Madian <im1996@fayoum.edu.eg>
2025-06-06 11:30:50 +0300

    [2932] [Time Beats: 100.00%] [Memory Beats: 38.90%] - LeetPush

---
+++ Misc/Easy/Maximum-Strong-Pair-XOR-I.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maximumStrongPairXor(vector<int>& nums) {
+        int n = nums.size();
+        int maxXor = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j < n; j++)
+                if(abs(nums[i] - nums[j]) <= min(nums[i], nums[j]))
+                    maxXor = max(maxXor, nums[i] ^ nums[j]);
+        return maxXor;
+    }
+};
