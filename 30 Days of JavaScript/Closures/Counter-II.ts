adf67db0da91580a3a5f975de9dd8478dfdf2167
Islam Madian <im1996@fayoum.edu.eg>
2025-06-28 02:49:50 +0300

    [2665] [Time Beats: 85.60%] [Memory Beats: 70.87%] - LeetPush

---
+++ 30 Days of JavaScript/Closures/Counter-II.ts
@@ -0,0 +1,29 @@
+type Counter = {
+    increment: () => number,
+    decrement: () => number,
+    reset: () => number,
+}
+
+function createCounter(init: number): Counter {
+    let temp: number = init;
+    return {
+        increment: () => {
+            return ++init;
+        },
+        decrement: () => {
+            return --init;
+        },
+        reset: () => {
+            init = temp;
+            return init;
+        }
+    };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
+ 
