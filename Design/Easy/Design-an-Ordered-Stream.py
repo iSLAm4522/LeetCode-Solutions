83dac0363940bbc0cb2c08cc82aceaa9a5957d6d
Islam Madian <islammadian798@gmail.com>
2025-09-21 17:20:15 +0300

    [1656] [Time Beats: 99.07%] [Memory Beats: 48.88%] - LeetPush

---
+++ Design/Easy/Design-an-Ordered-Stream.py
@@ -0,0 +1,13 @@
+class OrderedStream:
+
+    def __init__(self, n: int):
+        self.stream: List[Optional[str]] = [None] * n
+        self.ptr = 0
+
+    def insert(self, idKey: int, value: str) -> List[str]:
+        self.stream[idKey - 1] = value
+        ans = []
+        while self.ptr < len(self.stream) and self.stream[self.ptr] is not None:
+            ans.append(self.stream[self.ptr])
+            self.ptr += 1
+        return ans
