d135aafcd7584ed818d3e46cdd70bbd66fefd56e
Islam Madian <islammadian798@gmail.com>
2025-09-26 15:06:49 +0300

    [2414] [Time Beats: 36.16%] [Memory Beats: 98.47%] - LeetPush

---
+++ Misc/Medium/Length-of-the-Longest-Alphabetical-Continuous-Substring.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int longestContinuousSubstring(string s) {
+        int maxLen = 1;
+        int currLen = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s[i] - s[i - 1] == 1) {
+                currLen++;
+            } else {
+                maxLen = max(maxLen, currLen);
+                currLen = 1;
+            }
+        }
+        maxLen = max(maxLen, currLen);
+        return maxLen;
+    }
+};
